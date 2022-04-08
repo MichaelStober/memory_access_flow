f256f4094da8f3da41f0253a636a4b769ef3bc62
MichaelStober <98188038+MichaelStober@users.noreply.github.com>
2022-04-08 08:12:23 +0200

    Update MicroProgrammReadIndirectMemWord.vhd

--- ReadIndirectMemWord/MicroProgrammReadIndirectMemWord.vhd
+++ ReadIndirectMemWord/MicroProgrammReadIndirectMemWord.vhd
@@ -30,10 +30,10 @@ ARCHITECTURE MicroProgram_architecture OF MicroProgram IS
   TYPE Instruction_Array_Type is ARRAY (natural range <>) of Instruction_Type;
   CONSTANT Instruction_Array: Instruction_Array_Type := 
    ( --Lesen eines Speicherwortes mit indirekter Adressierugn
-	  --16#000# => (OPCODE => MICRO_LDA, OPERAND => X"0A00"), --Operand in ADR
-	  --16#001# => (OPCODE => MICRO_MVI, OPERAND => OPERAND_NONE), --Speicherwort von ADR in DAT
-	  --16#002# => (OPCODE => MICRO_MVA, OPERAND => OPERAND_NONE), --Kpoiere Inhalt von DAT in ADR
-	  --16#003# => (OPCODE => MICRO_MVI, OPERAND => OPERAND_NONE) --Speicherwort von DAT in ADR
+	  16#000# => (OPCODE => MICRO_LDA, OPERAND => X"0A00"), --Operand in ADR
+	  16#001# => (OPCODE => MICRO_MVI, OPERAND => OPERAND_NONE), --Speicherwort von ADR in DAT
+	  16#002# => (OPCODE => MICRO_MVA, OPERAND => OPERAND_NONE), --Kpoiere Inhalt von DAT in ADR
+	  16#003# => (OPCODE => MICRO_MVI, OPERAND => OPERAND_NONE) --Speicherwort von DAT in ADR
 
 	 );
 
